efc79a1c4ee58b0d90b3bf9b5335ef4ac4d26e8e
Maria Aparecidade Da silva de souza <wilkerjadson000@gmail.com>
2024-05-01 19:33:01 -0300

    Task - Backend User pronto - (01/05/24)

--- backend/src/database/conn/conn.ts
+++ backend/src/database/conn/conn.ts
@@ -2,20 +2,23 @@ import { Sequelize } from "sequelize";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const dbName = process.env.DB_NAME as string;
+const secret = process.env.SECRET!;
+
+const dbName = process.env.DB_MODEL as string;
 const dbUser = process.env.DB_USER as string;
 const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST
+
 
 const conn = new Sequelize(
-    'get_a_pet', // nome do bd
-    'root', // usuario do bd
-    '', // senha do bd
-    {
-        host: 'localhost', // localizacao do bd
-        dialect: 'mysql' // tipo do bd
-    }
-)
-console.log(dbName, '//////////////')
+  dbName, // nome do bd
+  dbUser, // usuario do bd
+  dbPassword, // senha do bd
+  {
+    host: dbHost, // localizacao do bd
+    dialect: "mysql", // tipo do bd
+  }
+);
 
 async () => {
   try {
